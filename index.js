83c0132e1f0a68da1121f90fd2f959bd11530d20
Ubuntu <ubuntu@vps-05574586.vps.ovh.net>
2024-06-03 15:25:32 +0000

    change port

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/send-email', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
